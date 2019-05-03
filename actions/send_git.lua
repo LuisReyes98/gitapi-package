0e01e1e1dd8889fab64f096c26aefbe01d557977
Luis Reyes <luisrogelio98@gmail.com>
2019-05-02 21:35:57 -0400

    refactor to all cloumns

--- actions/send_git.lua
+++ actions/send_git.lua
@@ -12,14 +12,14 @@ local query = "lighttouch"
 
 local my_response = settings.github_dummy_response.body
 
-
+local issue_columns = settings.github_dummy_response.issue_fields
 -- -- 
 -- -- 
 -- -- 
 local homepage = render("gitindex.html", {
   SITENAME = "GIT DISPLAY",
-  response = my_response,
   response_json = json.from_table(my_response),
+  issue_columns = issue_columns,
   issue_table_id = "my_super_original_id", -- be sure it is an string 
 })
 
