dbcd42c0f354696569356b8995f8f99f726a6ade
Luis Reyes <luisrogelio98@gmail.com>
2019-05-25 01:06:45 -0400

    working github request, data cleaning WIP

--- actions/send_git.lua
+++ actions/send_git.lua
@@ -3,46 +3,45 @@ priority = 1
 input_parameters = ["request"]
 
 -- 
---  GIT HUB request tests
+--  GIT HUB request
 -- 
 
+-- local query = "lighttouch"
 -- dummy data url https://api.github.com/search/issues?q={%22lighttouch%22}&page=1&per_page=3
+local cleaned_data = {}
 
-local query = "lighttouch"
+local URI_URL = "https://api.github.com/search/issues?q={%22lighttouch%22}&page=1&per_page=3"
 
-local my_response = settings.github_dummy_response.body
+local git_response = send_request({
+  uri = URI_URL,
+  method = "GET",
+  headers = {
+    ["content-type"] = "application/json",
+    ["accept"] = "application/vnd.github.v3+json", --git hub api v3 
+    ["Accept"] = "application/vnd.github.v3+json", --git hub api v3 
+  },
+})
 
-local issue_columns = settings.github_dummy_response.issue_fields
-
--- local all_labels = {}
-
--- for item in my_response.items do
---   for label in item.labels do
---     -- labels.concat(label.name)   
---     table.insert(all_labels,label.name)
---   end
--- end
-
-
-local json_file = fs.read_file('git.json')
-
-
--- local parsed_json = json.to_table(json_file)
--- log.debug(inspect(parsed_json))
+log.debug("Start of data cleaning")
+-- The cleaned data will be an array in which every position will have the following
+-- "Status","Title","Body","Subject","Labels","Creator","Locked","Assignees","Comments","Author association","Created at","Updated at","Closed at",
 
 
--- json_file = json.to_table(json_file)
 
+for key,value in pairs(git_response.body.items) do
+  table.insert(cleaned_data,{title = value.title}) 
+  -- log.debug('key:')
+  -- log.debug(k)
+  -- log.debug('value:')
+  -- log.debug(v)
+end
 -- -- 
 -- -- 
 -- -- 
 local homepage = render("gitindex.html", {
   SITENAME = "GIT DISPLAY",
-  response_json = json.from_table(my_response),
-  issue_columns = issue_columns,
   issue_table_id = "my_super_original_id", -- be sure it is an string 
-  json_file = json_file,
-  -- all_labels = json.from_table(all_labels),
+  git_response = json.from_table(cleaned_data),
 })
 
 return {
