5638a2702511a4345986319b974c32d840536069
Luis Reyes <luisrogelio98@gmail.com>
2019-05-28 18:14:57 -0400

    code refactor with functions

--- actions/send_git.lua
+++ actions/send_git.lua
@@ -5,36 +5,40 @@ input_parameters = ["request"]
 -- 
 --  GIT HUB request
 -- 
-
 -- local query = "lighttouch"
 -- dummy data url https://api.github.com/search/issues?q={%22lighttouch%22}&page=1&per_page=3
-local cleaned_data = {}
-
+local cleaned_data
 local URI_URL = "https://api.github.com/search/issues?q={%22lighttouch%22}&page=1&per_page=3"
 
-local git_response = send_request({
-  uri = URI_URL,
-  method = "GET",
-  headers = {
-    ["content-type"] = "application/json",
-    ["accept"] = "application/vnd.github.v3+json", --git hub api v3 
-    ["Accept"] = "application/vnd.github.v3+json", --git hub api v3 
-  },
-})
-
-log.debug("Start of data cleaning")
 -- The cleaned data will be an array in which every position will have the following
 -- "Status","Title","Body","Subject","Labels","Creator","Locked","Assignees","Comments","Author association","Created at","Updated at","Closed at",
 
+function load_github_api_data()
+  -- body
+  local data = {}
+  local response = send_request({
+    uri = URI_URL,
+    method = "GET",
+    headers = {
+      ["content-type"] = "application/json",
+      ["accept"] = "application/vnd.github.v3+json", --git hub api v3 
+      ["Accept"] = "application/vnd.github.v3+json", --git hub api v3 
+    },
+  })
+  for key,value in pairs(response.body.items) do
+    table.insert(data,{
+      title = value.title,
+      status = value.state,
+    })
 
+  end
 
-for key,value in pairs(git_response.body.items) do
-  table.insert(cleaned_data,{title = value.title}) 
-  -- log.debug('key:')
-  -- log.debug(k)
-  -- log.debug('value:')
-  -- log.debug(v)
+  return data
+
 end
+
+cleaned_data = load_github_api_data()
+
 -- -- 
 -- -- 
 -- -- 
